a197e7ca4d2b90eba7f874981dee2126f0124f04
Avinash <avinash.kumar@hashedin.com>
2019-12-19 15:23:03 +0530

    added

--- hello.sh
+++ hello.sh
@@ -1,5 +1,6 @@
 #!/bin/bash  
 echo "This is a shell script"
+sudo apt-get install curl
 curl https://sdk.cloud.google.com | bash
 exec -l $SHELL
 gcloud auth application-default login --no-launch-browser
